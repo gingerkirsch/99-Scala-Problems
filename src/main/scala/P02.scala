8f7df7cfc0bb770f86d54a922245de28010bee7f
honeyAndSw <honey.and.sw@gmail.com>
2018-08-03 16:48:27 +0200

    P02: penultimate

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -1,7 +1,8 @@
 package org.p99.scala
 
+/* Find the last but one element of a list. */
 object P02 {
 
-  // TODO
+  def penultimate[E](list: List[E]): E = list.reverse.tail.head
 
 }
--- src/test/scala/P02Spec.scala
+++ src/test/scala/P02Spec.scala
@@ -1,10 +1,25 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
-class P02Spec extends UnitSpec {
+class P02Spec extends UnitSpec with ListTestRunner[Int, Int] {
 
-  // TODO
+  "P02" should "work with lists that have more than 2 elements" in {
+    test(List(0,1)) should equal (0)
+    test(List(0,1,2,3,4)) should equal (3)
+  }
 
+  "P02" should "produce an Exception with 0-1 elements" in {
+    interceptAll {
+      test(List())
+    }
+
+    interceptAll {
+      test(List(0))
+    }
+  }
+
+  override def test(list: List[Int]): Int = P02.penultimate(list)
 }
 
