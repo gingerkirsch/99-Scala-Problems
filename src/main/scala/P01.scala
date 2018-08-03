dda430d5650524b46be6a68e7108f8635c831aa1
honeyAndSw <honey.and.sw@gmail.com>
2018-08-03 16:26:09 +0200

    P01: the last element of a list

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,7 +1,8 @@
 package org.p99.scala
 
+/* Find the last element of a list. */
 object P01 {
 
-  // TODO
+  def last[E](list: List[E]): E = list.reverse.head
 
 }
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -1,10 +1,23 @@
 package org.p99.scala
 
-import org.scalatest._
+import org.scalatest.Matchers._
 
 class P01Spec extends UnitSpec {
 
-  // TODO
+  "P01" should "return the last element of list" in {
+    test(List(0)) should equal (0)
+    test(List(0,1,2,3,4)) should equal (4)
+  }
 
+  "P01" should "produce Exception with empty inputs" in {
+    intercept[NoSuchElementException] {
+      test(List())
+    }
+
+    intercept[NoSuchElementException] {
+      test(Nil)
+    }
+  }
+
+  override def test(list: List[Int]): Int = P01.last(list)
 }
-
