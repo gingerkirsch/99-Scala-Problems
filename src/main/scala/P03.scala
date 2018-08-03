96310e39e59c030f10ed88c8a729ea043ee3ec83
honeyAndSw <honey.and.sw@gmail.com>
2018-08-03 17:06:39 +0200

    P03: nth

--- src/main/scala/P03.scala
+++ src/main/scala/P03.scala
@@ -1,7 +1,21 @@
 package org.p99.scala
 
+/* Find the Kth element of a list. */
 object P03 {
 
-  // TODO
+  def nth[E](n: Int, list: List[E]): E = {
+    // n=3 (!= 0) 1,2,3,4
+    // n=2 (!= 0) 2,3,4
+    // n=1 (!= 0) 3,4
+    // n=0 (== 0) --> 3
 
+    var tail = list
+    var pointer = n
+    while (pointer > 0 && tail.nonEmpty) {
+      tail = tail.tail
+      pointer -= 1
+    }
+
+    tail.head
+  }
 }
--- src/test/scala/P03Spec.scala
+++ src/test/scala/P03Spec.scala
@@ -1,10 +1,21 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
 class P03Spec extends UnitSpec {
+  import P03.nth
 
-  // TODO
+  "P03" should "work with non-empty list, n less than list's length" in {
+    nth(0, List(0,1,2,3)) should equal (0)
+    nth(1, List(0,1,2,3)) should equal (1)
+    nth(2, List(0,1,2,3)) should equal (2)
+    nth(3, List(0,1,2,3)) should equal (3)
+  }
 
+  "P03" should "produce an Exception when n >= list.size" in {
+    interceptAll {
+      nth(1, List(0))
+    }
+  }
 }
-
