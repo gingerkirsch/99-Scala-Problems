2ccfc556eb479f1217b069c7bd3eaeafb0c36da7
honeyAndSw <honey.and.sw@gmail.com>
2018-08-03 17:26:54 +0200

    P06: isPalindrome

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -1,7 +1,16 @@
 package org.p99.scala
 
+import scala.util.Try
+
+/* Find out whether a list is a palindrome. */
 object P06 {
 
-  // TODO
+  def isPalindrome[E](list: List[E]): Boolean = {
+    (list.size <= 1) || (Try {
+      val head = list.head
+      val last = P01.last(list)
+      (head == last) && isPalindrome(list.slice(1, list.size - 1))
+    } getOrElse false)
+  }
 
 }
--- src/test/scala/P06Spec.scala
+++ src/test/scala/P06Spec.scala
@@ -1,10 +1,28 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
-class P06Spec extends UnitSpec {
+class P06Spec extends UnitSpec with ListTestRunner[Int, Boolean] {
 
-  // TODO
+  "P06" should "return true on palindromic lists" in {
+    test(List(1)) should equal (true)
+    test(List(0,1,0)) should equal (true)
+    test(List(0,0)) should equal (true)
+    test(List(0,1,1,0)) should equal (true)
+  }
 
+  "P06" should "return false on non-palindromic lists" in {
+    test(List(0,1)) should equal (false)
+    test(List(0,1,2,3)) should equal (false)
+    test(List(0,1,0,1)) should equal (false)
+  }
+
+  "P06" should "return true on empty lists" in {
+    test(List()) should equal (true)
+    test(Nil) should equal (true)
+  }
+
+  override def test(list: List[Int]): Boolean = P06.isPalindrome(list)
 }
 
