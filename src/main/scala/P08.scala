20cb1ac111ab732eb863f689b3a410a3b04badd6
honeyAndSw <honey.and.sw@gmail.com>
2018-08-09 17:13:06 +0200

    P08: compress

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -1,7 +1,17 @@
 package org.p99.scala
 
+/* Eliminate consecutive duplicates of list elements.
+ * If a list contains repeated elements they should be replaced with a single copy of the element.
+ * The order of the elements should not be changed.
+ */
 object P08 {
 
-  // TODO
+  def compress[E](list: List[E]): List[E] = compareWithTarget(list.head, list.tail)
+
+  def compareWithTarget[E](target: E, list: List[E]): List[E] = {
+    if (list.isEmpty) List(target)
+    else if (target == list.head) compareWithTarget(target, list.tail)
+    else List(target) ++ compress(list)
+  }
 
 }
--- src/test/scala/P08Spec.scala
+++ src/test/scala/P08Spec.scala
@@ -1,10 +1,22 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
-class P08Spec extends UnitSpec {
+class P08Spec extends UnitSpec with ListTestRunner[Int, List[Int]] {
 
-  // TODO
+  "P08" should "return the same as the input when input has no dups" in {
+    test(List(0,1,2,3)) should equal (List(0,1,2,3))
+  }
 
+  "P08" should "remove dups and compress" in {
+    test(List(0,1,1,1,1,2,3)) should equal (List(0,1,2,3))
+    test(List(0,1,1,1,1,2,3,3,3,3)) should equal (List(0,1,2,3))
+    test(List(0,0,0,1,1,1,1,2,3)) should equal (List(0,1,2,3))
+  }
+
+  override def test(list: List[Int]): List[Int] = {
+    P08.compress(list)
+  }
 }
 
