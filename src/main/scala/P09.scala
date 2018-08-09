efbbe50f1d47d0f212c7e64d176c9f296f889840
honeyAndSw <honey.and.sw@gmail.com>
2018-08-09 17:16:23 +0200

    P09: pack

--- src/main/scala/P09.scala
+++ src/main/scala/P09.scala
@@ -1,7 +1,29 @@
 package org.p99.scala
 
+/* Pack consecutive duplicates of list elements into sublists.
+ * If a list contains repeated elements they should be placed in separate sublists.
+ */
 object P09 {
 
-  // TODO
+  def pack[E](list: List[E]): List[List[E]] = {
 
+    var list2: List[E] = list
+
+    var result: List[List[E]] = List()
+    var dups: List[E] = List()
+
+    while (list2.nonEmpty) {
+      val head = list2.head
+      list2 = list2.tail
+
+      if (dups.isEmpty || dups.head == head) { // extend current list
+        dups = dups :+ head
+      } else { // start new list
+        result = result :+ dups
+        dups = List(head)
+      }
+    }
+
+    if (dups.nonEmpty) result :+ dups else result
+  }
 }
--- src/test/scala/P09Spec.scala
+++ src/test/scala/P09Spec.scala
@@ -1,10 +1,28 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
-class P09Spec extends UnitSpec {
+class P09Spec extends UnitSpec with ListTestRunner[Int, List[List[Int]]] {
 
-  // TODO
+  "P09" should "pack duplicates of elements" in {
+    test(List(1,1,1,1, 2, 3,3, 1,1, 4, 5,5,5,5)) should equal (
+      List(
+        List(1,1,1,1), List(2), List(3,3), List(1,1), List(4), List(5,5,5,5)
+      ))
+  }
 
+  "P09" should "not pack a list that doesn't have duplicates" in {
+    test(List(1,2,3,4,5)) should equal (
+      List(List(1),List(2),List(3),List(4),List(5)
+    ))
+  }
+
+  "P09" should "not fail with empty lists" in {
+    test(List()) should equal (List())
+    test(Nil) should equal (List())
+  }
+
+  override def test(list: List[Int]): List[List[Int]] = P09.pack(list)
 }
 
