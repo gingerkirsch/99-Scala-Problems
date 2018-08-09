01fe98d309241521f3a225c9eac2b58eaa25df5e
honeyAndSw <honey.and.sw@gmail.com>
2018-08-09 16:59:39 +0200

    P07: flatten

--- src/main/scala/P07.scala
+++ src/main/scala/P07.scala
@@ -1,7 +1,20 @@
 package org.p99.scala
 
+/* Flatten a nested list structure. */
 object P07 {
 
-  // TODO
+  def flatten(list: List[Any]): List[Int] = {
+    if (list.isEmpty) Nil // end condition of recursion
+    else flattenOne(list.head) ++ flatten(list.tail)
+  }
 
+  /**
+    * @param elem can be an integer or a list
+    */
+  private def flattenOne(elem: Any): List[Int] = {
+    elem match {
+      case integer: Int => List(integer)
+      case _ => flatten(elem.asInstanceOf[List[Any]])
+    }
+  }
 }
--- src/test/scala/P07Spec.scala
+++ src/test/scala/P07Spec.scala
@@ -1,10 +1,28 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
-class P07Spec extends UnitSpec {
+class P07Spec extends UnitSpec with ListTestRunner[Any, List[Int]] {
 
-  // TODO
+  "P07" should "flatten 0-depth list" in {
+    test(List(0)) should equal (List(0))
+    test(List(0,1,2)) should equal (List(0,1,2))
+  }
 
+  "P07" should "flatten 1-depth list" in {
+    test(List(List(0))) should equal (List(0))
+    test(List(List(0), List(1))) should equal (List(0,1))
+  }
+
+  "P07" should "flatten mix of 0 or 1 depth list" in {
+    test(List(List(0),1,2)) should equal (List(0,1,2))
+    test(List(0,List(1),List(2))) should equal (List(0,1,2))
+  }
+
+  "P07" should "flatten more complicated list" in {
+    test(List(List(1, 1), 2, List(3, List(5, 8)))) should equal (List(1, 1, 2, 3, 5, 8))
+  }
+
+  override def test(list: List[Any]): List[Int] = P07.flatten(list)
 }
-
