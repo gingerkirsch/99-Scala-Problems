1db9f1a523ed4fbbb117b9922d0da982eb07c9a4
honeyAndSw <honey.and.sw@gmail.com>
2018-08-21 11:13:39 +0200

    P10: encode (run-length encoding)

--- src/main/scala/P10.scala
+++ src/main/scala/P10.scala
@@ -1,7 +1,15 @@
 package org.p99.scala
 
+/* Run-length encoding of a list.
+ * Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+ * Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of
+ * duplicates of the element E.
+ */
 object P10 {
 
-  // TODO
+  def encode[E](list: List[E]): List[(Int, E)] = {
+    P09.pack(list)
+      .map(elem => (elem.size, elem.head))
+  }
 
 }
--- src/test/scala/P10Spec.scala
+++ src/test/scala/P10Spec.scala
@@ -1,10 +1,20 @@
 package org.p99.scala
 
+import org.scalatest.Matchers._
 import org.scalatest._
 
-class P10Spec extends UnitSpec {
+class P10Spec extends UnitSpec with ListTestRunner[Int, List[(Int, Int)]] {
 
-  // TODO
+  "P10" should "do run-length encoding" in {
+    test(List(1,1,1,1, 2, 3,3, 1,1, 4, 5,5,5,5)) should equal (
+      List((4,1), (1,2), (2,3), (2,1), (1,4), (4,5))
+    )
 
+    test(List(1,2,3,4,5)) should equal (
+      List((1,1), (1,2), (1,3), (1,4), (1,5))
+    )
+  }
+
+  override def test(list: List[Int]): List[(Int, Int)] = P10.encode(list)
 }
 
