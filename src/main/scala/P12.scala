dd41c0b2f3343c9737313316cb75e1e89401c8fd
honeyAndSw <honey.and.sw@gmail.com>
2018-08-21 11:21:30 +0200

    P12: decode (run-length encoding)

--- src/main/scala/P12.scala
+++ src/main/scala/P12.scala
@@ -1,7 +1,15 @@
 package org.p99.scala
 
+/* Decode a run-length encoded list.
+ * Given a run-length code list generated as specified in problem P10,
+ * construct its uncompressed version.
+ */
 object P12 {
 
-  // TODO
+  def decode[E](encoded: List[(Int, E)]): List[E] = {
+    encoded.flatMap { case (n, elem) =>
+      for (_ <- 0 until n) yield elem
+    }
+  }
 
 }
--- src/test/scala/P12Spec.scala
+++ src/test/scala/P12Spec.scala
@@ -1,10 +1,20 @@
 package org.p99.scala
 
+import org.scalatest.Matchers._
 import org.scalatest._
 
-class P12Spec extends UnitSpec {
+class P12Spec extends UnitSpec with ListTestRunner[(Int, Int), List[Int]] {
 
-  // TODO
+  "P12" should "decode run-length encoded list" in {
+    test(List((4,1), (1,2), (2,3), (2,1), (1,4), (4,5))) should equal (
+      List(1,1,1,1,2,3,3,1,1,4,5,5,5,5)
+    )
 
+    test(List((1,1), (1,2), (1,3), (1,4), (1,5))) should equal (
+      List(1,2,3,4,5)
+    )
+  }
+
+  override def test(list: List[(Int, Int)]): List[Int] = P12.decode(list)
 }
 
