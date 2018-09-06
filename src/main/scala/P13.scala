e1ac97750992d326bd0baec9b44b656afe534bdd
honeyAndSw <honey.and.sw@gmail.com>
2018-09-06 14:40:00 +0200

    P13: encodeDirect (run-length encoding)

--- src/main/scala/P13.scala
+++ src/main/scala/P13.scala
@@ -1,7 +1,18 @@
 package org.p99.scala
 
+/* Run-length encoding of a list (direct solution).
+ * Implement the so-called run-length encoding data compression method directly.
+ * I.e. don't use other methods you've written (like P09's pack); do all the work directly.
+ */
 object P13 {
-
-  // TODO
-
+  def encodeDirect[E](list: List[E]): List[(Int, E)] = {
+    list.foldRight[List[(Int, E)]](Nil) { case (elem, accum) =>
+      if (accum.isEmpty) (1, elem) +: accum
+      else {
+        val head = accum.head
+        if (elem.equals(head._2)) (head._1 + 1, head._2) +: accum.tail
+        else (1, elem) +: accum
+      }
+    }
+  }
 }
--- src/test/scala/P13Spec.scala
+++ src/test/scala/P13Spec.scala
@@ -1,10 +1,20 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
-class P13Spec extends UnitSpec {
+class P13Spec extends UnitSpec with ListTestRunner[Int, List[(Int, Int)]] {
 
-  // TODO
+  "P13" should "do run-length encoding" in {
+    test(List(1,1,1,1, 2, 3,3, 1,1, 4, 5,5,5,5)) should equal (
+      List((4,1), (1,2), (2,3), (2,1), (1,4), (4,5))
+    )
 
+    test(List(1,2,3,4,5)) should equal (
+      List((1,1), (1,2), (1,3), (1,4), (1,5))
+    )
+  }
+
+  override def test(list: List[Int]): List[(Int, Int)] = P13.encodeDirect(list)
 }
 
