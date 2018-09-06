94294bc5cb6509eb8350b4fc84b191ac9ee51c91
honeyAndSw <honey.and.sw@gmail.com>
2018-09-06 14:50:58 +0200

    P15: duplicateN

--- src/main/scala/P15.scala
+++ src/main/scala/P15.scala
@@ -1,7 +1,14 @@
 package org.p99.scala
 
+/* Duplicate the elements of a list a given number of times.
+ */
 object P15 {
 
-  // TODO
+  def duplicateN[E](n: Int, list: List[E]): List[E] = {
+    list.foldRight[List[E]](Nil) { case (elem, accum) =>
+      val dups = for (_ <- 0 until n) yield elem
+      dups ++: accum
+    }
+  }
 
 }
--- src/test/scala/P15Spec.scala
+++ src/test/scala/P15Spec.scala
@@ -1,10 +1,17 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
-class P15Spec extends UnitSpec {
+class P15Spec extends UnitSpec with ListTestRunner[Int, List[Int]] {
 
-  // TODO
+  "P15" should "add two more elements" in {
+    test(List(1,2,3)) should equal (List(1,1,1,2,2,2,3,3,3))
+    test(List(1)) should equal (List(1,1,1))
+    test(List(1,1)) should equal (List(1,1,1,1,1,1))
+    test(Nil) should equal (Nil)
+  }
 
+  override def test(list: List[Int]): List[Int] = P15.duplicateN(3, list)
 }
 
