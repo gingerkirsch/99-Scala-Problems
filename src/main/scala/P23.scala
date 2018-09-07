a315c91136d38eb21375554522881c3c1d87eddc
honeyAndSw <honey.and.sw@gmail.com>
2018-09-07 15:55:47 +0200

    P23: randomSelect

--- src/main/scala/P23.scala
+++ src/main/scala/P23.scala
@@ -1,7 +1,20 @@
 package org.p99.scala
 
+import scala.util.Random
+
+/* Extract a given number of randomly selected elements from a list. */
 object P23 {
 
-  // TODO
+  def randomSelect[E](n: Int, list: List[E]): List[E] = randomSelect1(n, list, Nil)
+
+  private def randomSelect1[E](n: Int, list: List[E], result: List[E]): List[E] = {
+    if (n == 0) result
+    else {
+      // Be careful to choose 0 or positive integer
+      val randIdx = Math.abs(new Random().nextInt()) % list.length
+      val (subset, removed) = P20.removeAt(randIdx, list)
+      randomSelect1(n - 1, subset, result :+ removed)
+    }
+  }
 
 }
--- src/test/scala/P23Spec.scala
+++ src/test/scala/P23Spec.scala
@@ -1,10 +1,22 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
 class P23Spec extends UnitSpec {
 
-  // TODO
+  "P23" should "create random subset" in {
+    val input = List(1,2,3,4,5,6,7)
+    val n = 3
+
+    val output1 = P23.randomSelect(n, input)
+    val output2 = P23.randomSelect(n, input)
+
+    output1.size should equal (n)
+    output2.size should equal (n)
+
+    output1 shouldNot equal (output2)
+  }
 
 }
 
