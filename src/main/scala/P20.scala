f94be2086ee5dbd974e7ad1c50581d74e9b11477
honeyAndSw <honey.and.sw@gmail.com>
2018-09-07 15:29:59 +0200

    P20: removeAt

--- src/main/scala/P20.scala
+++ src/main/scala/P20.scala
@@ -1,7 +1,19 @@
 package org.p99.scala
 
+/* Remove the Kth element from a list.
+ * Return the list and the removed element in a Tuple.
+ * Elements are numbered from 0.
+ */
 object P20 {
 
-  // TODO
+  // Assumed that n is less than list.size
+  def removeAt[E](n: Int, list: List[E]): (List[E], E) = splitAndRemove(n, Nil, list)
 
+  private def splitAndRemove[E](n: Int, list1: List[E], list2: List[E]): (List[E], E) = {
+    if (n == 0) {
+      (list1 ++ list2.tail, list2.head)
+    } else {
+      splitAndRemove(n - 1, list1 :+ list2.head, list2.tail)
+    }
+  }
 }
--- src/test/scala/P20Spec.scala
+++ src/test/scala/P20Spec.scala
@@ -1,10 +1,17 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
 class P20Spec extends UnitSpec {
 
-  // TODO
+  "P20" should "remove an element and make a tuple" in {
+    val input = List(0,1,2,3,4)
+    P20.removeAt(0, input) should equal ((List(1,2,3,4), 0))
+    P20.removeAt(1, input) should equal ((List(0,2,3,4), 1))
+    P20.removeAt(2, input) should equal ((List(0,1,3,4), 2))
+    P20.removeAt(4, input) should equal ((List(0,1,2,3), 4))
+  }
 
 }
 
