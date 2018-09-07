78b3258c4b5910306e6bd4eaefbda22053c166b2
honeyAndSw <honey.and.sw@gmail.com>
2018-09-07 15:16:42 +0200

    P19: rotate

--- src/main/scala/P19.scala
+++ src/main/scala/P19.scala
@@ -1,7 +1,18 @@
 package org.p99.scala
 
+/* Rotate a list N places to the left. */
 object P19 {
 
-  // TODO
+  def rotate[E](n: Int, list: List[E]): List[E] = {
+    if (n == 0) {
+      list
+    } else {
+      // Make sure n1 > 0
+      val len = list.size
+      val n1 = if (n > 0) n % len else (n + len) % len
 
+      val (head, tail) = (list.head, list.tail)
+      rotate(n1 - 1, tail :+ head)
+    }
+  }
 }
--- src/test/scala/P19Spec.scala
+++ src/test/scala/P19Spec.scala
@@ -1,10 +1,24 @@
 package org.p99.scala
 
 import org.scalatest._
+import org.scalatest.Matchers._
 
 class P19Spec extends UnitSpec {
+  "P19" should "rotate to the right" in {
+    val input = List(1,2,3,4,5,6)
+    P19.rotate(0, input) should equal (List(1,2,3,4,5,6))
+    P19.rotate(1, input) should equal (List(2,3,4,5,6,1))
+    P19.rotate(2, input) should equal (List(3,4,5,6,1,2))
+    P19.rotate(3, input) should equal (List(4,5,6,1,2,3))
+    P19.rotate(7, input) should equal (List(2,3,4,5,6,1))
+  }
 
-  // TODO
-
+  "P19" should "rotate to the left" in {
+    val input = List(1,2,3,4,5,6)
+    P19.rotate(0, input) should equal (List(1,2,3,4,5,6))
+    P19.rotate(-1, input) should equal (List(6,1,2,3,4,5))
+    P19.rotate(-2, input) should equal (List(5,6,1,2,3,4))
+    P19.rotate(-3, input) should equal (List(4,5,6,1,2,3))
+  }
 }
 
